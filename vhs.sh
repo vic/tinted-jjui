31fc994a938a55423ad77d0d78bee04c06e7b68e
Victor Borja <vborja@apache.org>
2025-08-05 13:08:05 -0600

    update images
    gh

--- vhs.sh
+++ vhs.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 set -e -o pipefail
 ROOT=$PWD
-mkdir -p $ROOT/.tmp/$1/jjui/themes
+mkdir -p $ROOT/.tmp/$1/jjui/themes $ROOT/images/
 ln -sf $ROOT/themes/$1.toml $ROOT/.tmp/$1/jjui/themes/$1.toml
 echo -e "[ui]\ntheme='$1'" >$ROOT/.tmp/$1/jjui/config.toml
 (
   cd $ROOT/.tmp/$1
-  vhs -q $ROOT/vhs.tape
-  url="$PWD/jjui.gif"
-  if test -n "${CI:-}"; then
-    url="$(vhs publish -q jjui.gif)"
+  JJUI_REPO="${2:-${JJUI_REPO:-$ROOT}}"
+  cp $ROOT/vhs.tape vhs.tape
+  echo >source.tape
+  if grep '# Scheme variant: light' $ROOT/themes/$1.toml >&2 >/dev/null; then
+    echo 'Set Theme "zenwritten_light"' >>source.tape
+  else
+    echo 'Set Theme "zenwritten_dark"' >>source.tape
   fi
-  echo "$url"
+  cat <<EOF >>source.tape
+Env JJUI_CONF_DIR "${PWD}"
+Env JJUI_REPO "${JJUI_REPO}"
+EOF
+  vhs -q vhs.tape
+  mv "$ROOT/.tmp/$1/jjui.png" $ROOT/images/$1.png
+  echo "$ROOT/images/$1.png"
   tq='```'
   cat <<EOF >$ROOT/.tmp/$1.md
 ### [${1}](./themes/${1}.toml)
@@ -22,7 +31,7 @@ theme = "${1}"
 ${tq}
 
 <p align="center">
-  <img title="${1}" alt="${1}" src="${url}" />
+  <img title="${1}" alt="${1}" src="images/${1}.png" />
 </p>
 
 EOF
